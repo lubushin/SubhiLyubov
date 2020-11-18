39693c43f35b3f25b4f435507ef1cc3030e21847
Lyubov Ubushin <Lyubau1124@gmail.com>
2020-11-18 17:52:24 -0500

    message3

--- src/main.java
+++ src/main.java
@@ -3,5 +3,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hi, how are you?");
         System.out.println("I am fine, thank you, how about you");
+        System.out.println("I am fine , too, thank you ");
     }
 }
